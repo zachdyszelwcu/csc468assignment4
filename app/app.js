dc814cbf04d93ce31d17525fe4ac0518f5b77159
zachdyszelwcu <zachdyszel@gmail.com>
2026-03-27 20:24:28 -0400

    added shark stuff

--- app/app.js
+++ app/app.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 
+
+
+const db = require('./db')
+const sharks = require('./routes/sharks');
+
+
 const path = __dirname + '/views/';
 const port = 8080;
 
@@ -18,8 +24,12 @@ router.get('/sharks',function(req,res){
   res.sendFile(path + 'sharks.html');
 });
 
+
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path));
-app.use('/', router);
+app.use('/sharks', sharks);
 
 app.listen(port, function () {
   console.log('Example app listening on port 8080!')
